c42714a0ed0c24100d744196c5029f724b8afd08
Brei <Brei@Brei-PC>
2015-10-05 11:45:57 +0200

    cambio euros dolares

---
+++ src/boletin2_3/Boletin2_3.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2_3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Brei
+ */
+public class Boletin2_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float euros, cambio, total;
+        System.out.println("Introduce cantidad de Euros a cambiar");
+        Scanner cantidadEuros = new Scanner(System.in);
+        euros = cantidadEuros.nextFloat();
+        System.out.println("Introduce el valor actual de los dolares en euros");
+        Scanner cambioDolares = new Scanner(System.in);
+        cambio = cambioDolares.nextFloat();
+        total = euros*cambio;
+        System.out.println(total);
+        
+       
+    }
+    
+}
